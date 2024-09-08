cd2a8605015279fa543b747d34eb5ba7d31f9a9f
Valentine Samuel <valentinesamuel2580@gmail.com>
2024-09-08 17:52:59 +0100

    feat: added s3 upload [no ci]

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,8 +1,24 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
-const exec = require('@actions/exec');
+const core = require("@actions/core");
+const github = require("@actions/github");
+const exec = require("@actions/exec");
 
-function run(){
-    console.log('Deploying to S3');
+function run() {
+  const bucket = core.getInput("bucket", {
+    trimWhitespace: true,
+    required: true,
+  });
+  const bucketRegion = core.getInput("bucket-region", {
+    trimWhitespace: true,
+    required: true,
+  });
+  const distFolder = core.getInput("dist-folder", {
+    trimWhitespace: true,
+    required: true,
+  });
+
+  const s3Url = `s3://${bucket}`;
+  exec.exec(`aws s3 sync ${distFolder} ${s3Url}  --region ${bucketRegion}`);
+
+  core.notice(`Deploying to bucket: ${bucket}`);
 }
 run();
