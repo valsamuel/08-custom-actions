b909d5621922d16905b2681e6ad4d39c1155b59c
Valentine Samuel <valentinesamuel2580@gmail.com>
2024-09-08 17:26:38 +0100

    added github js action

---
+++ .github/actions/deploy-s3-javascript/main.js
@@ -0,0 +1,8 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const exec = require('@actions/exec');
+
+function run(){
+    console.log('Deploying to S3');
+}
+run();
